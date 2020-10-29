4073b80bdb6e30cfc75fd140fc3d3144c7d0fe32
alexi <Alexis-Gu-Ar>
2020-10-28 23:05:06 -0600

    refactor into function

--- main.go
+++ main.go
@@ -7,6 +7,22 @@ import (
 	"sort"
 )
 
+func writeStringsToFile(strings []string, fileName string) {
+	file, err := os.Create(fileName)
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i, str := range strings {
+		file.WriteString(str)
+		if i < len(strings)-1 {
+			file.WriteString("\n")
+		}
+	}
+}
+
 func main() {
 	n := 5
 	scanner := bufio.NewScanner(os.Stdin)
@@ -17,35 +33,10 @@ func main() {
 		scanner.Scan()
 		strings[i] = scanner.Text()
 	}
+
 	sort.Strings(strings)
-
-	fileAcs, err := os.Create("asecendente.txt")
-	defer fileAcs.Close()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for i, str := range strings {
-		fileAcs.WriteString(str)
-		if i < len(strings)-1 {
-			fileAcs.WriteString("\n")
-		}
-	}
+	writeStringsToFile(strings, "asecendente.txt")
 
 	sort.Sort(sort.Reverse(sort.StringSlice(strings)))
-
-	fileDes, err := os.Create("descendente.txt")
-	defer fileDes.Close()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for i, str := range strings {
-		fileDes.WriteString(str)
-		if i < len(strings)-1 {
-			fileDes.WriteString("\n")
-		}
-	}
+	writeStringsToFile(strings, "descendente.txt")
 }
